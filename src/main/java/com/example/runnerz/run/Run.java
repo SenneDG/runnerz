abd6e5234cd2d5c39754e410e17065f2357f9e93
Senne <senne.degreef@student.kuleuven.be>
2024-10-04 17:02:21 +0200

    rest api implementation

--- src/main/java/com/example/runnerz/run/Run.java
+++ src/main/java/com/example/runnerz/run/Run.java
@@ -2,12 +2,24 @@ package com.example.runnerz.run;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 public record Run(
     Integer id,
+    @NotEmpty
     String title,
     LocalDateTime startedOn,
     LocalDateTime endedOn,
+    @Positive
     Integer miles,
     Location location
 
-) {}
+) {
+
+    public Run {
+        if(endedOn.isBefore(startedOn)) {
+            throw new IllegalArgumentException("End date must be after start date");
+        }
+    }
+}
