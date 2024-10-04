65eef1ecb683ea178acd2b099ba5777a0263d6ed
Senne <senne.degreef@student.kuleuven.be>
2024-10-04 22:27:36 +0200

    Module 5: RestClient

--- src/main/java/com/example/runnerz/Application.java
+++ src/main/java/com/example/runnerz/Application.java
@@ -1,6 +1,7 @@
 package com.example.runnerz;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +9,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import com.example.runnerz.run.Location;
 import com.example.runnerz.run.Run;
+import com.example.runnerz.user.User;
+import com.example.runnerz.user.UserHttpClient;
 
 @SpringBootApplication
 public class Application {
@@ -20,12 +26,23 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	// @Bean
-	// CommandLineRunner runner() {
-	// 	return args -> {
-	// 		Run run = new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plusHours(1), 5, Location.OUTDOOR);
-	// 		log.info("Run: {}", run);
-	// 	};
-	// }
+	@Bean
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client) {
+		return args -> {
+			List<User> users = client.findAll();
+			System.out.println("Users: " + users.size());
+
+			User user = client.findById(1);
+			System.out.println("User: " + user);
+		};
+
+	}
 
 }
