71ef9d4acf3bd1ea80863662715be9649a3e4cff
Senne <senne.degreef@student.kuleuven.be>
2024-10-04 15:28:40 +0200

    Initial Commit

---
+++ src/main/java/com/example/runnerz/run/Run.java
@@ -0,0 +1,13 @@
+package com.example.runnerz.run;
+
+import java.time.LocalDateTime;
+
+public record Run(
+    Integer id,
+    String title,
+    LocalDateTime startedOn,
+    LocalDateTime endedOn,
+    Integer miles,
+    Location location
+
+) {}
